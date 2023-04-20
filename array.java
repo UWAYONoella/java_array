fc94912be86d92cdc9ec3e49e321f5bd6ac32839
UWAYONoella <noellawayo2019@gmail.com>
2023-04-20 17:08:24 +0200

    readme

---
+++ array.java
@@ -0,0 +1,10 @@
+// example
+class array{
+    public static void main(String args[]) {
+        int []array1=new int[5];
+        array1[0]=10;
+        System.out.println(array1);
+
+        
+    }
+}
